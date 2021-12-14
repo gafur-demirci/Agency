f04dcf52b3ca00197d8d4c47444cd50c66117b50
gafur-demirci <gafur.gs.68gmail.com>
2021-12-14 20:54:57 +0300

    Nodemailer added and working

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,4 @@
+const nodemailer = require('nodemailer');
 const Category = require("../models/Category");
 const Client = require('../models/Client');
 const Portfolio = require('../models/Portfolio');
@@ -26,4 +27,47 @@ exports.getAdminPage = async (req, res) => {
         portfolios,
         teams
     });
+}
+
+exports.sendEmail = async (req,res) => {
+    try {
+        const mailTemplate = `
+        <h1> Mail Detail</h1>
+        <ul>
+            <li>Name : ${req.body.name}</li>
+            <li>Email : ${req.body.email}</li>
+            <li>Phone : ${req.body.phone}</li>
+        </ul>
+        <h1>Message Detail</h1>
+        <p>${req.body.message}</p>
+        `;
+
+        const transporter = nodemailer.createTransport({
+            host : 'smtp.gmail.com',
+            port : 465,
+            secure : true,
+            auth : {
+                user : 'gafur.gs.68@gmail.com',
+                pass : 'dxuncocgbfoeqcdk',
+            },
+        });
+
+        const info = await transporter.sendMail({
+            from : '"Agency Contact" <gafur.gs.68@gmail.com>',
+            to : 'gafur.gs.68@gmail.com',
+            subject : 'Agency Project New Message From Contact',
+            html : mailTemplate,
+        });
+
+        console.log('Message sent: %s', info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.status(200).redirect('contact');
+
+    } catch (error) {
+        console.log(error);
+    }
 }
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.route('/').get(pageController.getIndexPage);
 router.route('/about').get(pageController.getAboutPage);
 router.route('/contact').get(pageController.getContactPage);
+router.route('/contact').post(pageController.sendEmail);
 router.route('/admin').get(pageController.getAdminPage);
 
 module.exports = router;
